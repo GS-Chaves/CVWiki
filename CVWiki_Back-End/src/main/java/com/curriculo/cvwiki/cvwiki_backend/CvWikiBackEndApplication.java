1b3efc55719e03282edbdfb04da8cadc94c4d66a
Guilherme Chaves <guilherme.chaves66@gmail.com>
2024-07-20 21:41:37 -0300

    Criando o Back-End

---
+++ CVWiki_Back-End/src/main/java/com/curriculo/cvwiki/cvwiki_backend/CvWikiBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.curriculo.cvwiki.cvwiki_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvWikiBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvWikiBackEndApplication.class, args);
+    }
+
+}
